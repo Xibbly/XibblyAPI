b573af15c2ffd4bac94e07b53c1e12d3cbef912c
SzymonG110 <gornikowskiszymon@gmail.com>
2022-06-14 20:35:40 +0200

    permission type fix

--- src/types/website.type.ts
+++ src/types/website.type.ts
@@ -1,4 +1,4 @@
-import {NextFunction, Request, Response} from "express";
+import {NextFunction, Request, Response} from 'express'
 import UserModelType from './models/user.modelType'
 
 export interface RouteArguments {
@@ -9,18 +9,19 @@ export interface RouteArguments {
 
 export default interface RouteType {
     route: string
-    permissions?: string[]
+    permissions?: Array<'*' | 'admin_panel' | 'view_users' | 'manage_users' | 'view_tokens' | 'manage_tokens'>
+    tokenPermissions?: Array<'*'>
     disabled?: boolean
     mustLogin?: boolean
 
     get: (data: RouteArguments) => any
     post: (data: RouteArguments) => any
-
 }
 
 declare module 'express-session' {
     interface SessionData {
         user: Omit<UserModelType, 'password' | 'ip'>
+
         [x: string]: any
     }
 }
