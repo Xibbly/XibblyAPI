48a4e48c7b27accbe785a309c65c02ab2a0e96ef
SzymonG110 <gornikowskiszymon@gmail.com>
2022-07-03 19:06:55 +0200

    working sending globalchat messages

--- src/website/routes/api/globalchatSend.route.ts
+++ src/website/routes/api/globalchatSend.route.ts
@@ -18,38 +18,21 @@ export default {
         if (!await new TokensHandler().getToken(data.token))
             return res.status(401).send('Unauthorized')
 
+        // @todo webhook database
         const webhooks = ['https://discord.com/api/webhooks/992423581436874792/mUUzP8YJYSxaP8zXWJXSZZp3z2gR02FdWIxk3HY7_MsU_iX40NIp32cBCRCb0eMhpD_p']
         const numberOfChannels = webhooks.length
 
         let sendedTo = 0
-        const fixedFiles = []
-
-        if (data.filesUrl && data.filesUrl[0])
-            for (const file of data.filesUrl) {
-                fixedFiles.push({
-                    description: file.split('/').pop(),
-                    filename: file
-                })
-            }
 
         for (const webhook of webhooks) {
             const response = await axios.post(process.env.WEBHOOK_API_URL!, {
-                'token': process.env.WEBHOOK_API_TOKEN,
-                'webhookUrl': webhook,
-                'webhookData': {
+                token: process.env.WEBHOOK_API_TOKEN,
+                webhookUrl: webhook,
+                webhookData: {
                     username: `${data.tag} | ${data.userId}`,
-                    avatar_url: data.avatarUrl,
+                    avatarUrl: data.avatarUrl,
                     content: data.content,
-                    // attachments: [{
-                    //     id: 0,
-                    //     filename: 'w3duR07.png',
-                    //     url: 'https://i.imgur.com/w3duR07.png'
-                    // }],
-                    files: [{
-                        id: 0,
-                        attachment: 'https://i.imgur.com/w3duR07.png',
-                        name: 'w3duR07.png'
-                    }]
+                    filesUrl: data.filesUrl
                 }
             }, {
                 headers: {
@@ -57,10 +40,8 @@ export default {
                 }
             })
 
-            if (response.data.status == 200)
+            if (response.status == 200)
                 sendedTo++
-            else
-                console.log(webhook, response.data)
         }
 
         return res.send({
