12c874ff625c3cda6e4945ee54bdd6b2d56a0134
SzymonG110 <gornikowskiszymon@gmail.com>
2022-07-19 21:40:03 +0200

    better cookies style

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -4,6 +4,8 @@ class Cookies {
 
         document.getElementById('cookies').style.display = 'none'
         localStorage.setItem('cookiesClosed', 'true')
+        document.getElementById('title').classList.remove('blur')
+        document.getElementById('content').classList.remove('blur')
 
     }
 
@@ -12,6 +14,8 @@ class Cookies {
             document.getElementById('cookies').style.display = 'none'
         } else {
             document.getElementById('cookies').style.display = 'block'
+            document.getElementById('title').classList.add('blur')
+            document.getElementById('content').classList.add('blur')
         }
     }
 
